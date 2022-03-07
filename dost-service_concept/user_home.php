bc4019db96d026daee8267702676496f698524a7
Cris-io <67158549+Cris-io@users.noreply.github.com>
2022-03-07 20:58:42 +0800

    import files to github

---
+++ dost-service_concept/user_home.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <title>DOST Service Request - Home</title>
+
+    
+
+
+    <!-- My styles for this template -->
+    <link href="style.css" rel="stylesheet">
+    <script src="script.js" defer></script>
+
+</head>
+<body>
+<!-- Header Page  -->
+    <header class="header">
+<!-- Left menu buttons -->
+        <div class="left-menu">
+            <img class="menu-logo-item" src="img/DOST_log.png">
+            <button class="menu-icon-btn" data-menu-icon-btn>
+                <img class="menu-logo-item" src="icons/png-files/menu.png">
+            </button>
+        </div>
+<!-- Right menu buttons -->
+        <div class="right-menu" >
+          
+          <div class="dropdown" data-dropdown-menu>
+            <button class="menu-profile-btn">
+              <img class="menu-logo-item" src="img/undraw_profile.svg" data-dropdown-button>
+            </button>
+            <div class="dropdown-menu" >
+              <a class="link"> <img class="link-icon" src="icons/png-files/cogs.png" alt="Settings.png"> Settings</a> 
+              <a class="link"> <img class="link-icon" src="icons/png-files/exit.png" alt="Logout.png"> Logout</a>
+            </div>
+
+          </div>
+        </div>
+    </header>
+
+<!-- Page Content Container -->
+    <div class="container">
+        <aside class="sidebar open" data-slider>
+<!-- Sidebar top menu-->
+            <div class="top-sidebar">
+            <div class="sidebar-list">
+                <div class="sidebar-list-item">
+                <a href="" class="sidebar-link active">
+                    <img class="sidebar-icon" src="icons/png-files/home.png">
+                    <div class="hidden-sidebar">Home</div>
+                </a>
+                </div>
+            </div>
+            </div>
+
+            
+            <hr class="sidebar-linebreak">
+<!-- Sidebar middle menu -->
+            <div class="middle-sidebar">
+                <ul class="sidebar-list">
+
+                    <li class="sidebar-list-item">
+                      <a href="#" class="sidebar-link">
+                        <img class="sidebar-icon" src="icons/png-files/add-files.png">
+                        <div class="hidden-sidebar">Request</div>
+                      </a>
+                    </li>
+                    <li class="sidebar-list-item">
+                      <a href="#" class="sidebar-link">
+                        <img class="sidebar-icon" src="icons/png-files/alarm-clock.png">
+                        <div class="hidden-sidebar">History</div>
+                      </a>
+                    </li>
+                    <li class="sidebar-list-item">
+                      <a href="#" class="sidebar-link">
+                        <img class="sidebar-icon" src="icons/png-files/agenda.png">
+                        <div class="hidden-sidebar">View Request</div>
+                      </a>
+                    </li>
+                    <li class="sidebar-list-item">
+                      <a href="#" class="sidebar-link">
+                        <img class="sidebar-icon" src="icons/png-files/information.png">
+                        <div class="hidden-sidebar">About</div>
+                      </a>
+                    </li>
+                    <li class="sidebar-list-item">
+                      <a href="#" class="sidebar-link">
+                        <img class="sidebar-icon" src="icons/png-files/cog.png">
+                        <div class="hidden-sidebar">Settings</div>
+                      </a>
+                    </li>
+                    <li class="sidebar-list-item">
+                      <a href="#" class="sidebar-link">
+                        <img class="sidebar-icon" src="icons/png-files/exit.png">
+                        <div class="hidden-sidebar">Logout</div>
+                      </a>
+                    </li>
+                  </ul>
+            </div>
+<!-- Sidebar bottom menu -->
+            <div class="bottom-sidebar">
+              <ul class="sidebar-list">
+                <li class="sidebar-list-item">
+                  <a href="#" class="sidebar-link">
+                    <img class="sidebar-icon" src="icons/png-files/envelope.png">
+                  </a>
+                </li>
+                <li class="sidebar-list-item">
+                  <a href="#" class="sidebar-link">
+                    <img class="sidebar-icon" src="icons/png-files/world.png">
+                  </a>
+                </li>
+                <li class="sidebar-list-item">
+                  <a href="#" class="sidebar-link">
+                    <img class="sidebar-icon" src="icons/png-files/facebook-oval.png">
+                  </a>
+                </li>
+              </ul>
+            </div>
+        </aside>
+        <main class="content">
+          <div class="content-menu">
+            <div class="item-1">
+              <h3 class="header-title">Request Interface</h3>
+              <ul class="link-menu">
+                <li ><a href="">
+                  <div class="btn-link">
+                    Add Repair Request
+                    <img src="icons/png-files/">
+                  </div></a>
+                </li>
+                <li ><a href="">
+                  <div class="btn-link">
+                    Add ICT Job Request
+                    <img src="icons/png-files/">
+                  </div></a></li>
+              </ul>
+            </div>
+          </div>
+        </main>
+    </div>
+
+
+
+    
+</body>
+</html>
